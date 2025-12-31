0ecdfbf94be38e3c0688f4cb39cf8567ab0fbd3d
Aarush Burra <iamagameryay@gmail.com>
2025-12-30 23:32:52 -0500

    Created the detector.py file in order to detect when trades should be executed/closed, created tests_regime.py to test the system before papertrading practice

--- regime_trader/regime/detector.py
+++ regime_trader/regime/detector.py
@@ -1,6 +1,5 @@
 """Market regime detection."""
 import pandas as pd
-import numpy as np
 
 
 class RegimeDetector:
@@ -10,27 +9,12 @@ class RegimeDetector:
         self.vix_threshold = vix_threshold
     
     def detect_vix_regime(self, vix_level: float) -> str:
-        """Simple VIX threshold-based regime detection.
-        
-        Args:
-            vix_level: Current VIX level
-            
-        Returns:
-            'low_vol' or 'high_vol'
-        """
+        """Simple VIX threshold-based regime detection."""
         return 'high_vol' if vix_level >= self.vix_threshold else 'low_vol'
     
     def label_dataframe(self, df: pd.DataFrame, 
                         vix_col: str = 'Close_VIX') -> pd.DataFrame:
-        """Add regime labels to dataframe.
-        
-        Args:
-            df: DataFrame with VIX data
-            vix_col: Name of VIX column
-            
-        Returns:
-            DataFrame with 'Regime' column added
-        """
+        """Add regime labels to dataframe."""
         df = df.copy()
         df['Regime'] = df[vix_col].apply(self.detect_vix_regime)
         return df
